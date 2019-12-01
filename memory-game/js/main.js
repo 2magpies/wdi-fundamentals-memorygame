ac18b09bab2d23eea2dff83a5de9484e2e338d72
Daphne Mintz <daphnemintz@daphneslmbp2019.attlocal.net>
2019-11-30 22:27:01 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,14 +2,24 @@ console.log("Up and running!");
 
 let cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) { alert("You found a match!")
 
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+function flipCard(cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) { alert("You found a match!");
+checkForMatch();
 }
 else { alert("Sorry, try again.")
 }
+}
+
+flipCard(0);
+flipCard(2);
